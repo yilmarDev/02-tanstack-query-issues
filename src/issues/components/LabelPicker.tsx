11061320fe3d37b2dc36f4c5b35648ac80f07843
Yilmar Ospina <ospina.yilmar@gmail.com>
2023-12-16 18:24:41 -0500

    tanstack implemented Axios implemented Label interface created

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,14 +1,28 @@
+import { useQuery } from '@tanstack/react-query';
+import { githubApi } from '../../api/githubApi';
+import { Label } from '../../interfaces/label';
+
+const getLabels = async (): Promise<Label[]> => {
+  const { data } = await githubApi.get<Label[]>('/labels');
+  console.log('Data form axios', data[0].name);
+  return data;
+};
 
 export const LabelPicker = () => {
+  const labelQuery = useQuery({
+    queryKey: ['labels'],
+    queryFn: getLabels,
+    refetchOnWindowFocus: false,
+  });
+
   return (
     <div>
-        <span 
-            className="badge rounded-pill m-1 label-picker"
-            style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
-        >
-            Primary
-        </span>
-        
+      <span
+        className="badge rounded-pill m-1 label-picker"
+        style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
+      >
+        Primary
+      </span>
     </div>
-  )
-}
+  );
+};
