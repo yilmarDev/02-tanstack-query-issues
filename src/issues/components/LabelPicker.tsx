4534bb81e4bd3a7c6e36850bddda842318ad6466
Yilmar Ospina <ospina.yilmar@gmail.com>
2023-12-17 15:39:52 -0500

    Section 3 finished Selected labels implemented LabelPicker updated

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -4,7 +4,12 @@ import { Label } from '../../interfaces/label';
 import { useLabels } from '../../hooks/useLabels';
 import { LoadingIcon } from '../../shared/components/LoadingIcon';
 
-export const LabelPicker = () => {
+type Props = {
+  selectedLabels: string[];
+  onchange: (labelName: string) => void;
+};
+
+export const LabelPicker = ({ selectedLabels, onchange }: Props) => {
   const labelsQuery = useLabels();
 
   if (labelsQuery.isLoading) return <LoadingIcon />;
@@ -14,7 +19,10 @@ export const LabelPicker = () => {
       {labelsQuery.data?.map((label, index) => (
         <div key={index}>
           <span
-            className="badge rounded-pill m-1 label-picker"
+            className={`badge rounded-pill m-1 label-picker ${
+              selectedLabels.includes(label.name) && `label-active`
+            }`}
+            onClick={() => onchange(label.name)}
             style={{
               border: `1px solid #${label.color}`,
               color: `#${label.color}`,
