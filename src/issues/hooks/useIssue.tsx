80b799bd6ab72517639fd5b699b4989830c330fd
Yilmar Ospina <ospina.yilmar@gmail.com>
2023-12-18 16:07:49 -0500

    Data from useIssue implemented

--- src/issues/hooks/useIssue.tsx
+++ src/issues/hooks/useIssue.tsx
@@ -1,9 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
 import { githubApi } from '../../api/githubApi';
 import { Issue } from '../../interfaces';
+import { sleep } from '../../helpers.tsx/sleep';
 
 const getIssue = async (issueNumber: number): Promise<Issue> => {
   const { data } = await githubApi.get<Issue>(`/issues/${issueNumber}`);
+  await sleep(2);
   console.log('Issues: ', data);
   return data;
 };
